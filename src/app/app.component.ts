cceff8db8ceb9c5e668d1856beecd1dff3285f49
Bruno de Souza Lobo Almeida <brunoslalmeida@gmail.com>
2025-01-15 00:12:53 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'susep-extractor-angular 2';
+  title = 'susep-extractor-angular 3';
 }
