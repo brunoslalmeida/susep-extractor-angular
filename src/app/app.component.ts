bf962e966e66016e894952323e4059fd3f8ba79d
Bruno de Souza Lobo Almeida <brunoslalmeida@gmail.com>
2025-01-16 02:21:08 -0300

    adding base form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { initFlowbite } from 'flowbite';
+
+import { Observable } from 'rxjs';
+
+interface ICompany {
+  code: string;
+  name: string;
+}
+
+interface IType {
+  code: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'susep-extractor-angular 3';
+export class AppComponent implements OnInit {
+  private baseUrl = 'https://susep-extractor.bruno-s-l-almeida.workers.dev/';
+
+  companies: ICompany[] = [];
+  types: IType[] = [];
+
+  susepForm = new FormGroup({
+    company: new FormControl(''),
+    type: new FormControl(''),
+  });
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    initFlowbite();
+    this.http
+      .get<{
+        companies: ICompany[];
+        types: IType[];
+      }>(this.baseUrl)
+      .subscribe(data => {
+        this.companies = data.companies;
+        this.types = data.types
+      });
+  }
+
+  onSubmit() {
+    console.log(this.susepForm.value);
+  }
+
+  title = 'Susep Extractor';
 }
